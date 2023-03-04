514ae67a5ec960b6a3055aa3be64dafe770c9dc6
jeff <jeffreyjohn102@gmail.com>
2023-03-03 21:00:29 -0500

    Add Spring JDBC dependency and modify Cart JDBC class

--- bakery/src/main/java/com/bakery/dao/CartDao.java
+++ bakery/src/main/java/com/bakery/dao/CartDao.java
@@ -1,4 +1,9 @@
 package com.bakery.dao;
 
+import com.bakery.model.Cart;
+
 public interface CartDao {
+
+    public Cart get(long id);
+
 }
--- bakery/src/main/java/com/bakery/dao/JdbcCartDao.java
+++ bakery/src/main/java/com/bakery/dao/JdbcCartDao.java
@@ -1,4 +1,42 @@
 package com.bakery.dao;
 
+import com.bakery.model.Cart;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
+
+@Component
 public class JdbcCartDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public JdbcCartDao() {
+        this.jdbcTemplate = new JdbcTemplate();
+    }
+
+    // Retrieve a customers cart based on their ID
+    public Cart getCustomerCart(long id) {
+        
+        Cart returnedCart = null;
+
+        String sql = "SELECT ";
+
+        SqlRowSet rowSet = jdbcTemplate.queryForRowSet(sql);
+
+        if (rowSet.next()) {
+            returnedCart = mapRowToCart(rowSet);
+        }
+
+        return returnedCart;
+    }
+
+    public Cart mapRowToCart(SqlRowSet rowSet) {
+        Cart mappedCart = null;
+
+        mappedCart.setCartId(rowSet.getLong(""));
+
+        return mappedCart;
+    }
+
 }
