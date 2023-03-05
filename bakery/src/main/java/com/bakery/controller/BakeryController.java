5c12c2caf2bfc9a01f0184a1206d0b389dce949c
jeff <jeffreyjohn102@gmail.com>
2023-03-04 22:01:13 -0500

    Define Rest Controller and route for getting all items

--- bakery/src/main/java/com/bakery/controller/BakeryController.java
+++ bakery/src/main/java/com/bakery/controller/BakeryController.java
@@ -1,16 +1,33 @@
 package com.bakery.controller;
 
-import com.bakery.dao.ItemDao;
-import com.bakery.dao.JdbcItemDao;
+import com.bakery.dao.*;
+import com.bakery.model.Item;
 import org.apache.commons.dbcp2.BasicDataSource;
-import com.bakery.dao.CartDao;
-import com.bakery.dao.JdbcCartDao;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
 
+@RestController
+@RequestMapping(path = "/api/bakery")
 public class BakeryController {
 
-    //TODO: remove
+    private CustomerDao customerDao;
+    private CartDao cartDao;
+    private ItemDao itemDao;
 
+    public BakeryController(CustomerDao customerDao, CartDao cartDao, ItemDao itemDao) {
+        this.customerDao = customerDao;
+        this.cartDao = cartDao;
+        this.itemDao = itemDao;
+    }
+
+    @RequestMapping(path = "/items", method = RequestMethod.GET)
+    public List<Item> getItems() {
+        return itemDao.getAllItems();
+    }
 
 }
