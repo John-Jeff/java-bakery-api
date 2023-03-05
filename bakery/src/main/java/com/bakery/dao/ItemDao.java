cb9cc02912125fb7b7d868a67e9a70cd523838c9
jeff <jeffreyjohn102@gmail.com>
2023-03-04 20:08:27 -0500

    Add more methods to ItemDao interface

--- bakery/src/main/java/com/bakery/dao/ItemDao.java
+++ bakery/src/main/java/com/bakery/dao/ItemDao.java
@@ -6,6 +6,12 @@ import java.util.List;
 
 public interface ItemDao {
 
+    public Item getItemById(long itemId);
+
+    public List<Item> getAllItems();
+
     public List<Item> getCartItems(long cartId);
+
+    public Item createItem(Item newItem);
 
 }
